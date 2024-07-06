c86ca8479628d91e708590211dd50e9d6e40c642
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-06 13:39:35 -0400

    Commit

--- CPractice/main.c
+++ CPractice/main.c
@@ -10,11 +10,20 @@ void myFunction () {
     mySecondFunction();
 }
 
+void calculateSum() {
+    int x = 5;
+    int y = 17;
+    int sum = x + y;
+    printf("The sum of x + y = %d \n", sum);
+}
+
 int main()
 {
     printf("Hello, World!\n");
 
     myFunction();
+
+    calculateSum();
 
     return 0;
 }
