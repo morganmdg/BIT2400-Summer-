6d086af5c3cf42c92db6e0770c050ba6b77db5ea
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-06 14:13:29 -0400

    Commit

--- CPractice/main.c
+++ CPractice/main.c
@@ -4,10 +4,10 @@
 //https://www.w3schools.com/c/c_functions.php
 
 void mySecondFunction () {
-    printf("Second test message\n");
+    printf("Second test for my function\n");
 }
 void myFunction () {
-    printf("Testing my function\n");
+    printf("First test for my function\n");
     mySecondFunction();
 }
 
@@ -15,22 +15,21 @@ void calculateSum() {
     int x = 5;
     int y = 17;
     int sum = x + y;
-    printf("The sum of x + y = %d \n", sum);
+    printf("If x is 5 and y is 17, the sum of x + y = %d \n", sum);
 }
 
 void nameFunction (char name[]) {
-printf("Hello %s\n", name);
+printf("%s\n", name);
 };
 
 int main()
 {
-    printf("Hello, World!\n");
+    printf("Hello ");
+    nameFunction("Morgan");
 
     myFunction();
 
     calculateSum();
-
-    nameFunction("Morgan");
 
     return 0;
 }
