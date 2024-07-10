2402b267c90c68b220da820f41eb5041d963c5b2
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-10 09:49:12 -0400

    Update PaintJob.h

--- A01/PaintJob.h
+++ A01/PaintJob.h
@@ -7,17 +7,19 @@
 
 #include "stdio.h"
 
+//unsigned short int cNumCans = 0, cPaintCost = 0;
+
 class PaintJob {
 public:
     // Add your methods here
     void userBudget();
-    unsigned short int NumCans;
-    float CoatCost;
-    float PaintNeeded;
+    unsigned short int NumCans = 0;
+    float CoatCost = 0.0f;
+    float PaintNeeded = 0.0f;
 private:
-    float Height, Width, Length, Area;
-    float Budget;
-    float CanPrice;
+    float Height = 0.0f, Width = 0.0f, Length = 0.0f, Area = 0.0f;
+    float Budget = 0.0f;
+    float CanPrice = 0.0f;
 };
 
 #endif //PAINT_H
