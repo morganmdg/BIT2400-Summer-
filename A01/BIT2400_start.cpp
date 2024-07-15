5efe4038f6b2d8f7c72c1f61150bfa2b0bdb576e
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-15 18:11:16 -0400

    Update

--- A01/BIT2400_start.cpp
+++ A01/BIT2400_start.cpp
@@ -7,24 +7,30 @@ using namespace std;
 
 int main()
 {
-    float Height, Width, Length, Area;
+    float Height, Width, Length;
+    float Budget, CanPrice;
 
     PaintJob cpaintJob;
     /*PaintJob cpaintCalc;*/
 
-    /*Prompt user to enter budget
+    //Prompt user to enter budget
     printf("Enter your budget: ");
-    scanf("%f", &Budget);*/
+    scanf("%f", &Budget);
 #
     //Prompt user to enter wall dimensions
-    printf("Enter wall heigth (feet): ");
-    scanf_s("%f", &Height);
+    printf(" Enter wall heigth (feet): ");
+    scanf_s(" %f", &Height);
 
     printf("Enter wall width (feet): ");
-    scanf_s("%f", &Width);
+    scanf_s(" %f", &Width);
 
     printf("Enter wall length (feet): ");
-    scanf_s("%f", &Length);
+    scanf_s(" %f", &Length);
+
+    //Calculate and outupt area
+    float Area = cpaintJob.getArea(Height, Width, Length);
+
+    printf("Wall area: %.2f ft^2\n", Area);
 
     /*Prompt user to enter price of the can of paint
     printf("Enter price of can of paint: %d\n", CanPrice);*/
@@ -32,9 +38,6 @@ int main()
     //Output
     printf("\n******************************\n\n");
 
-    cpaintJob.getArea(Height, Width, Length);
-
-    printf("Wall area: %.2f ft^2\n", Area);
 
     //Output
     printf("\n******************************\n\n");
