5995812dda6a88793e0856844aa551b1ef65e2ae
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-21 15:12:59 -0400

    Update

---
+++ MidtermPractice5/Instruments.h
@@ -0,0 +1,21 @@
+//
+// Created by morga on 2024-07-21.
+//
+
+#ifndef INSTRUMENTS_H
+#define INSTRUMENTS_H
+
+#include <string>
+#pragma once
+
+
+class Instruments {
+
+    string genericAttribute;
+    Instruments();
+
+};
+
+
+
+#endif //INSTRUMENTS_H
