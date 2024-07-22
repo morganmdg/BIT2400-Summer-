5453ff969f09b22edd0d26f65aacde532e6eada2
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-21 23:12:29 -0400

    Update birdHouse.h

--- Morgan_Gardiner_A02/birdHouse.h
+++ Morgan_Gardiner_A02/birdHouse.h
@@ -9,13 +9,13 @@
 
 class birdHouse {
 public:
-    birdHouse (string Filename); //Constructor should open the file and set the wordList
+    birdHouse (std::string Filename); //Constructor should open the file and set the wordList
     ~birdHouse();
-    string userGuess; //String to store the user guesses Initalize to dashes
+    std::string userGuess; //String to store the user guesses Initalize to dashes
 private:
-    string* wordList; //list of words read from the file
-    string randomWord; //A random word from the list
-    usigned short int NumWords; //Number of words read from the file
+    std::string* wordList; //list of words read from the file
+    std::string randomWord; //A random word from the list
+    unsigned short int NumWords; //Number of words read from the file
     unsigned short int numGuess; //Current number of user guesses
     char uInput; //user input
     unsigned short int wordLength; //Variable to store the length of the random word to guess
