2ff569541cf5400eb2db7489e4e44062864abc1f
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-22 19:12:28 -0400

    Update
    Trying to open file with filepath

--- Morgan_Gardiner_A02/birdHouse.cpp
+++ Morgan_Gardiner_A02/birdHouse.cpp
@@ -12,22 +12,22 @@ using namespace std;
 
 birdHouse::birdHouse(std::string Filename)
 {
-    string wordList;
-    ifstream file;
-    file.open(Filename);
+    string wordList;  //string that will hold data read from file
+    ifstream birdfile;  //creating an object of ifstream
+    birdfile.open(Filename); //open file for reading
 
-    if (!file.is_open()) //check if file was opened with no errors
+    if (!birdfile.is_open()) //check if file was opened with no errors
     {
       cout << "Error opening file";
         exit(1);
     }
 
-    while(!file.eof())
+    while(!birdfile.eof())
     {
-        getline(file, wordList);  //get line of text from the file
-        cout << wordList << endl;
+        getline(birdfile, wordList);  //get line of text from the file
+        cout << wordList << endl; //output line of text that was read
     }
 
-    file.close();
-    
+    birdfile.close(); //close the file
+
 }
--- Morgan_Gardiner_A02/main.cpp
+++ Morgan_Gardiner_A02/main.cpp
@@ -20,6 +20,28 @@ int main()
 
     string filename = filenameStr;
 
-    birdHouse startbirdHouse(filename);
+    string filepathStr = "Morgan_Gardiner_A02/cmake-build-debug/";
+
+    string namepathStr = filenameStr + filepathStr;
+
+   // birdHouse startbirdHouse(std::string filename);
+
+    string wordList;  //string that will hold data read from file
+    ifstream birdfile;  //creating an object of ifstream
+    birdfile.open(namepathStr); //open file for reading
+
+    if (!birdfile.is_open()) //check if file was opened with no errors
+    {
+        cout << "Error opening file";
+        exit(1);
+    }
+
+    while(!birdfile.eof())
+    {
+        getline(birdfile, wordList);  //get line of text from the file
+        cout << wordList << endl; //output line of text that was read
+    }
+
+    birdfile.close(); //close the file
 
 }
