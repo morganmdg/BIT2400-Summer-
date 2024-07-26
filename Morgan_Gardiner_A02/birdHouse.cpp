54e3ee8f1895254c77bd98f108e3255aceb3a1d9
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-25 22:33:23 -0400

    Update birdHouse.cpp

--- Morgan_Gardiner_A02/birdHouse.cpp
+++ Morgan_Gardiner_A02/birdHouse.cpp
@@ -191,6 +191,25 @@ void birdHouse::wordGuess()
 void birdHouse::printbirdHouse()
 {
 //Add switch case
+    switch(wrongGuess)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        break;
+    case 8:
+        break;
+    }
 }
 
 
