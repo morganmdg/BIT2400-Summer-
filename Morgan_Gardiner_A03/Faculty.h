1c8e860163ab6babe6082a594fa6bab57ee0afda
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-30 12:17:27 -0400

    Update Faculty.h

--- Morgan_Gardiner_A03/Faculty.h
+++ Morgan_Gardiner_A03/Faculty.h
@@ -1,15 +1,19 @@
 //
 // Created by morga on 2024-07-30.
 //
-
+#include "Person.h"
 #ifndef FACULTY_H
 #define FACULTY_H
+#include <string>
 
 
-
-class Faculty {
+class Faculty : public Person {
+public:
+    void printInfo();
+    void userInput();
 protected:
-    
+    std::string DepName;
+    bool FacultyType;
 };
 
 
