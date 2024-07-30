e5a1d4d65e16e1f92dd44d3822d3519babb92735
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-30 14:03:27 -0400

    Update Graduate.cpp

--- Morgan_Gardiner_A03/Graduate.cpp
+++ Morgan_Gardiner_A03/Graduate.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+//TAType method returns if TA is Full or Half TA based on hours assigned
 bool Graduate::TAType()
 {
     if (hoursAssigned > 65)
@@ -17,6 +18,50 @@ bool Graduate::TAType()
     {
         TA = true; //True = HalfTime TA
     }
-
     return TA;
+}
+
+//The calcGPA method returns student’s letter grade based on grade percentage
+string Graduate::calcGPA()
+{
+    string letterGrade;
+    int gradePercentage;
+
+    if(gradePercentage == 95)
+    {
+        letterGrade = "A+";
+    }
+    else if(gradePercentage == 90)
+    {
+        letterGrade = "A";
+    }
+    else if(gradePercentage == 85)
+    {
+        letterGrade = "A-";
+    }
+    else if(gradePercentage == 80)
+    {
+        letterGrade = "B+";
+    }
+    else if(gradePercentage == 75)
+    {
+        letterGrade = "B";
+    }
+    else if(gradePercentage == 70)
+    {
+        letterGrade = "B-";
+    }
+    else if(gradePercentage == 65)
+    {
+        letterGrade = "C+";
+    }
+    else if(gradePercentage == 60)
+    {
+        letterGrade = "C";
+    }
+    else if(gradePercentage < 50)
+    {
+        letterGrade = "F";
+    }
+    return letterGrade;
 }
