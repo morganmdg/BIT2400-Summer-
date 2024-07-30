b519379f121e86f1bb3d43f509ab3dd12be2b32b
morganmdg <92071955+morganmdg@users.noreply.github.com>
2024-07-30 12:34:08 -0400

    Update PartTime.h

--- Morgan_Gardiner_A03/PartTime.h
+++ Morgan_Gardiner_A03/PartTime.h
@@ -1,16 +1,19 @@
 //
 // Created by morga on 2024-07-30.
 //
-
+#include "Faculty.h"
 #ifndef PARTTIME_H
 #define PARTTIME_H
 
 
-
-class PartTime {
-
+class PartTime : public Faculty {
+public:
+    void printInfo();
+    void userInput();
+    float salary();
+protected:
+    int hoursWorked;
+    float hourlyRate;
 };
-
-
 
 #endif //PARTTIME_H
